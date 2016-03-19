d0b5d2465dd04d557856966ecb0cbaeb536bd2d0
ccrowhurstram <ccrowhurst@ramplc.com>
2016-03-19 19:35:20 +0000

    extend earthquake dashboard with table

--- app/earthquakes/app.ts
+++ app/earthquakes/app.ts
@@ -1,18 +1,40 @@
 const QUAKE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp';
 
+function getRowFromEvent(table: HTMLTableElement, eventName: string){
+    return Rx.Observable.fromEvent<UIEvent>(table, eventName).filter(evt => {
+        let el = evt.target as HTMLElement;
+        return el.tagName === "TD" && !!el.parentElement.id;
+    })
+    .map(evt => (event.target as HTMLElement).parentElement)
+    .distinctUntilChanged();
+}
+
 function isJsonpSuccessResponse<T>(data: any): data is Rx.DOM.JsonpSuccessResponse<T> {
     return data && data.response != null;
 }
 
-var quakesFeed = Rx.Observable.interval(5000)
+function makeRow(props: EarthQuakes.EarthQuakeProperties) {
+  var row = document.createElement('tr');
+  row.id = props.net + props.code;
+
+  var date = new Date(props.time);
+  var time = date.toString();
+  [props.place, props.mag, time].forEach(function(text: any) {
+    var cell = document.createElement('td');
+    cell.textContent = text;
+    row.appendChild(cell);
+  });
+
+  return row;
+}
+
+var quakes = Rx.Observable.interval(5000)
     .flatMap(() => {
         return Rx.DOM.jsonpRequest<EarthQuakes.Dataset>({
             url: QUAKE_URL,
             jsonpCallback: "eqfeed_callback"
         }).retry(3);
-    });
-
-let quakes = quakesFeed
+    })
     .flatMap((data) => {
         if (isJsonpSuccessResponse(data)) {
             return Rx.Observable.from(data.response.features);
@@ -20,50 +42,68 @@ let quakes = quakesFeed
             return Rx.Observable.from([] as EarthQuakes.EarthQuake[]);
         }
     })
-    .distinct(quake => quake.properties["code"])
+    .distinct(quake => quake.properties["code"]).publish();
+
+let quakesMapInfo = quakes
     .map(quake => {
         return {
+            id: quake.id,
             lat: quake.geometry.coordinates[1],
             lng: quake.geometry.coordinates[0],
             size: quake.properties.mag * 10000
         };
     });
+    
+    
 
 function run() {
     var map = L.map('map').setView([43.804133, -120.554201], 7); //<!-- (3) -->
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map); //<!-- (4) -->
     
-    experiment();
+    let codeLayers: { [name: string]: string } = {};
+    let quakeLayer = L.layerGroup([]).addTo(map);
 
-    quakes.subscribe(quake => {
-        L.circle([quake.lat, quake.lng], quake.size).addTo(map);
+
+    quakesMapInfo.subscribe(quake => {
+        let circle = L.circle([quake.lat, quake.lng], quake.size).addTo(map);
+        quakeLayer.addLayer(circle);
+        codeLayers[quake.id] = quakeLayer.getLayerId(circle).toString();
     });
-}
-
-function experiment() {
-    var id = 1;
-    var delayed = Rx.Observable.range(1, 4).map(value => {
-        return {
-            id: id++
-        }
-    }).delay(1000);
-
-    var asyncSource = new Rx.AsyncSubject<{ id: number }>();
-    delayed.subscribe(asyncSource);
-
-
-    asyncSource.subscribe(x => {
-        console.log(`Subscriber 1: ${x.id}`);
-    });
-
-
-    setTimeout(() => {
-        console.log(`Before subscription 2 created`);
-        asyncSource.subscribe(x => {
-            console.log(`Subscriber 2: ${x.id}`);
+    
+    var table = document.getElementById('quakes_info') as HTMLTableElement;
+    quakes
+        .pluck<EarthQuakes.EarthQuakeProperties>('properties')
+        .map(makeRow)
+        .bufferWithTime(500)
+        .filter(rows => rows.length > 0)
+        .map(rows => {
+            var fragment = document.createDocumentFragment();
+            rows.forEach(function(row) {
+                fragment.appendChild(row);
+            });
+            return fragment;
+        })
+        .subscribe(function(fragment) { 
+            table.appendChild(fragment); 
         });
-        console.log(`After subscription 2 created`);
-    }, 1000);
+        
+    let rowEnter$ = getRowFromEvent(table, "mouseover").pairwise();
+    rowEnter$.subscribe(rows => {
+        var prevCircle = quakeLayer.getLayer(codeLayers[rows[0].id]);
+        var currCircle = quakeLayer.getLayer(codeLayers[rows[1].id]);
+
+        prevCircle.setStyle({ color: '#0000ff' });
+        currCircle.setStyle({ color: '#ff0000' });
+    });
+    
+    
+    let quakeSelected$ = getRowFromEvent(table, "click");
+    quakeSelected$.subscribe(row => {
+       var circle = quakeLayer.getLayer(codeLayers[row.id]);
+       map.panTo(circle.getLatLng()); 
+    });
+    
+    quakes.connect();
 }
 
 export default run;
--- app/earthquakes/earthquake.d.ts
+++ app/earthquakes/earthquake.d.ts
@@ -1,14 +1,21 @@
 declare namespace EarthQuakes {
     
+    export interface EarthQuakeProperties {
+        code: string,
+        mag: number,
+        net: string,            
+        place: string,
+        time: string,
+        [name: string]: any
+    }
+    
     export interface EarthQuake {
         geometry: {
             type: string;
             coordinates: number[]
         },
-        properties: {
-            mag: number,
-            [name: string]: any
-        }
+        id: string,
+        properties: EarthQuakeProperties
     }
     
     export interface Dataset {
